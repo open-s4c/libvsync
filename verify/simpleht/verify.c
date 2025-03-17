44954aee8d95068b22726ff5aed2bed519337b7d
Lilith Oberhauser <lilith.oberhauser@huawei.com>
2025-03-17 17:07:05 +0200

    simpleht update
    Signed-off-by: Lilith Oberhauser <lilith.oberhauser@huawei.com>

--- verify/simpleht/verify.c
+++ verify/simpleht/verify.c
@@ -1,54 +1,37 @@
 /*
- * Copyright (C) Huawei Technologies Co., Ltd. 2024. All rights reserved.
+ * Copyright (C) Huawei Technologies Co., Ltd. 2024-2025. All rights reserved.
  * SPDX-License-Identifier: MIT
  */
 
 #define NTHREADS 3U
 #include <test/map/isimple.h>
-#include <test/thread_launcher.h>
+#include <test/boilerplate/test_case.h>
 
-void t0(vsize_t);
-void t1(vsize_t);
-void t2(vsize_t);
-void pre(void);
-void post(void);
-
-#ifdef TEST_CASE
-    #include TEST_CASE
-#else
-    #error "no test case was defined"
-#endif
-
-
-void *
-run(void *args)
+void
+reg(vsize_t tid)
 {
-    vsize_t tid = (vsize_t)(vuintptr_t)args;
     imap_reg(tid);
-    switch (tid) {
-        case 0:
-            t0(tid);
-            break;
-        case 1:
-            t1(tid);
-            break;
-        case 2:
-            t2(tid);
-            break;
-        default:
-            break;
-    }
+}
+void
+dereg(vsize_t tid)
+{
     imap_dereg(tid);
-    return NULL;
+}
+
+void
+init(void)
+{
+    imap_init();
+}
+void
+fini(void)
+{
+    imap_destroy();
 }
 
 int
 main(void)
 {
-    imap_init();
-    pre();
-    launch_threads(NTHREADS, run);
-    post();
-    imap_destroy();
+    tc();
     return 0;
 }
