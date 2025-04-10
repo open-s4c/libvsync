da4380ea686979b7b5d29d3788982f6ed41cdfa5
Lilith Oberhauser <lilith.oberhauser@huawei.com>
2025-04-10 18:10:30 +0300

    Recent update
    Signed-off-by: Lilith Oberhauser <lilith.oberhauser@huawei.com>

---
+++ verify/thread/mutex_slim_liveness.c
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) Huawei Technologies Co., Ltd. 2024-2025. All rights reserved.
+ * SPDX-License-Identifier: MIT
+ */
+
+#define VFUTEX_LIVENESS
+#define NTHREADS 2
+
+#include <test/boilerplate/lock.h>
+#include <vsync/thread/mutex/slim.h>
+
+vmutex_t lock;
+
+void
+acquire(vuint32_t tid)
+{
+    V_UNUSED(tid);
+    vmutex_acquire(&lock);
+}
+
+void
+release(vuint32_t tid)
+{
+    V_UNUSED(tid);
+    vmutex_release(&lock);
+}
